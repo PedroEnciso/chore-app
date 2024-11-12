9ddf8aa625ae7d3b943a7706e005a9f30975bd8e
Pedro Enciso <penciso@ucsd.edu>
2024-11-12 10:28:23 -0800

    initial commit

---
+++ app/lib/data.ts
@@ -0,0 +1,27 @@
+export const chores = [
+  {
+    id: 1,
+    name: "Vacuum family room",
+    frequency: 604800000,
+    description: "Turn off the beater bar.",
+    frequency_description: "Once a week",
+    last_completed: 1731125348984,
+  },
+  {
+    id: 2,
+    name: "Vacuum and dut stairs",
+    frequency: 604800000,
+    description: "Vacuum the stairs and upstairs hallway. Wipe the hand rail.",
+    frequency_description: "Once a week",
+    last_completed: 1731125348984,
+  },
+  {
+    id: 3,
+    name: "Wipe downstairs baseboards",
+    frequency: 604800000,
+    description:
+      "Usea microfiber cloth to clean the baseboards. Be sure to go behind furniture, go in the pantry and go in the powder room.",
+    frequency_description: "Once a week",
+    last_completed: 1731125348984,
+  },
+];
---
+++ app/lib/types.ts
@@ -0,0 +1,8 @@
+export interface Chore {
+  id: number;
+  name: string;
+  frequency: number;
+  description: string;
+  frequency_description: string;
+  last_completed: number;
+}
