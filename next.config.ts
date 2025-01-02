31ceec0a01c6ef55ba8741eb58d4f0bb61c07d1b
Pedro Enciso <penciso@ucsd.edu>
2025-01-02 11:16:36 -0800

    / redirects to /chores

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/chores",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
