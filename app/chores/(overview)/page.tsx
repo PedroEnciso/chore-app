e971c3bd75a1110ee71b8dbb682ba4a23c8bc9c0
Pedro Enciso <penciso@ucsd.edu>
2025-01-03 15:12:16 -0800

    updated html

--- app/chores/(overview)/page.tsx
+++ app/chores/(overview)/page.tsx
@@ -1,17 +1,20 @@
 import { Suspense } from "react";
 import ChoreList from "@/app/ui/chores/ChoreList";
-import TypographyH1 from "@/components/typography/h1";
+import { TypographyH1 } from "@/components/typography";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 export default async function Home() {
   return (
     <>
       <header className="mt-10 w-11/12 mx-auto flex flex-col gap-3">
         <TypographyH1>Enciso Chores</TypographyH1>
-        <Button variant="outline" className="w-fit">
-          Add a chore
-        </Button>
+        <Link href="/chores/create">
+          <Button variant="outline" className="w-fit">
+            Add a chore
+          </Button>
+        </Link>
       </header>
-      <main className="w-11/12 mx-auto">
+      <main className="w-11/12 mx-auto mt-8">
         <Suspense fallback={<p>Loading chores...</p>}>
           <ChoreList />
         </Suspense>
