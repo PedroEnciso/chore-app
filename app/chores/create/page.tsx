b6f41ce7a6e9a4c6e92e8741429efcf84b0f86d2
Pedro Enciso <penciso@ucsd.edu>
2025-01-03 15:12:26 -0800

    created component

--- app/chores/create/page.tsx
+++ app/chores/create/page.tsx
@@ -1,3 +1,23 @@
+import ChoreForm from "@/app/ui/chores/ChoreForm";
+import { TypographyH1, TypographyH2 } from "@/components/typography";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default async function Page() {
-  return <>Create page</>;
+  return (
+    <>
+      <header className="mt-10 w-11/12 mx-auto flex flex-col gap-3">
+        <TypographyH1>New Chore</TypographyH1>
+        <Link href="/chores">
+          <Button variant="outline" className="w-fit">
+            Back to chores
+          </Button>
+        </Link>
+      </header>
+      <main className="w-11/12 mx-auto mt-8">
+        <ChoreForm />
+      </main>
+      {/* <p>"My idea of housework is to sweep the room with a glance."</p> */}
+    </>
+  );
 }
