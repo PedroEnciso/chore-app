ea35c6fb3c52ed62bd788a8bee70b7e6ff8dfdf0
Pedro Enciso <penciso@ucsd.edu>
2025-01-09 17:10:23 -0800

    use Header and ChoreContainer

--- app/chores/(overview)/page.tsx
+++ app/chores/(overview)/page.tsx
@@ -1,25 +1,22 @@
 import { Suspense } from "react";
-import ChoreList from "@/app/ui/chores/ChoreList";
+import ChoreContainer from "@/app/ui/chores/ChoreContainer";
 import { TypographyH1 } from "@/components/typography";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { Providers } from "./provider";
+import Header from "@/app/ui/chores/Header";
+
 export default async function Home() {
   return (
     <>
-      <header className="mt-10 w-11/12 mx-auto flex flex-col gap-3">
-        <TypographyH1>Enciso Chores</TypographyH1>
-        <Link href="/chores/create">
-          <Button variant="outline" size="sm" className="w-full md:w-fit">
-            Add a chore
-          </Button>
-        </Link>
-      </header>
-      <main className="w-11/12 mx-auto mt-8">
-        <Suspense fallback={<p>Loading chores...</p>}>
-          <ChoreList />
-        </Suspense>
-      </main>
-      {/* <p>"My idea of housework is to sweep the room with a glance."</p> */}
+      <Providers>
+        <Header page="chores" />
+        <main className="w-11/12 mx-auto mt-8">
+          <Suspense fallback={<p>Loading chores...</p>}>
+            <ChoreContainer />
+          </Suspense>
+        </main>
+      </Providers>
     </>
   );
 }
