9c9c43d11cb7165a784a8a4dd1dc6c12e5d4f3b9
Pedro Enciso <penciso@ucsd.edu>
2025-01-09 17:12:29 -0800

    choreChore and member

--- app/lib/types.ts
+++ app/lib/types.ts
@@ -1,9 +1,24 @@
-export interface Chore {
+interface ChoreCore {
   id: number;
   name: string;
   frequency: number;
-  description: string;
   frequency_description: string;
+  description: string;
   last_completed: Date;
   due_date: Date;
+}
+
+export interface Chore extends ChoreCore {
+  member_last_completed?: number;
+}
+
+export interface ChoreWithMember extends ChoreCore {
+  member_name: string;
+}
+
+// export type Member = "ped" | "Daniel" | "Anisa" | "mom" | "dad";
+
+export interface Member {
+  id: number;
+  name: string;
 }
