b0e084d8ef5be50a6d673d719064311131e4e34a
Pedro Enciso <penciso@ucsd.edu>
2025-01-11 11:25:49 -0800

    use Providers and Header

--- app/chores/create/page.tsx
+++ app/chores/create/page.tsx
@@ -1,23 +1,17 @@
 import ChoreForm from "@/app/ui/chores/ChoreForm";
+import Header from "@/app/ui/chores/Header";
 import { TypographyH1, TypographyH2 } from "@/components/typography";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { Providers } from "../(overview)/provider";
 
 export default async function Page() {
   return (
-    <>
-      <header className="mt-10 w-11/12 mx-auto flex flex-col gap-3">
-        <TypographyH1>New Chore</TypographyH1>
-        <Link href="/chores">
-          <Button variant="outline" className="w-fit">
-            Back to chores
-          </Button>
-        </Link>
-      </header>
+    <Providers>
+      <Header page="new" />
       <main className="w-11/12 mx-auto mt-8">
         <ChoreForm />
       </main>
-      {/* <p>"My idea of housework is to sweep the room with a glance."</p> */}
-    </>
+    </Providers>
   );
 }
