a07d7c76aac69b11c8867701624d7fe926da30e8
Pedro Enciso <penciso@ucsd.edu>
2025-01-11 11:43:42 -0800

    added try catch

--- seedDB.ts
+++ seedDB.ts
@@ -5,21 +5,25 @@ import dotenv from "dotenv";
 dotenv.config();
 
 async function seedDB() {
-  // first clear the db of all data
-  // const data = await sql`
-  //   TRUNCATE chores;
-  // `;
-  // console.log("Finished", data);
-
-  // add all data from
-  chores.forEach(async (chore) => {
+  try {
+    // first clear the chores and activity of all data
     await sql`
-    INSERT INTO chores
-    (name, frequency, description, frequency_description, last_completed, due_date)
-    VALUES
-    (${chore.name}, ${chore.frequency}, ${chore.description}, ${chore.frequency_description}, ${chore.last_completed}, ${chore.due_date})
-    `;
-  });
+  TRUNCATE chores CASCADE;
+`;
+
+    // add all data from
+    chores.forEach(async (chore) => {
+      await sql`
+  INSERT INTO chores
+  (name, frequency, description, frequency_description, last_completed, due_date)
+  VALUES
+  (${chore.name}, ${chore.frequency}, ${chore.description}, ${chore.frequency_description}, ${chore.last_completed}, ${chore.due_date})
+  `;
+    });
+    console.log("Success!");
+  } catch (error) {
+    console.log("Failure", error);
+  }
 }
 
 seedDB();
