ba21356dc7a1ae97ac27c489882e090b6ec6c501
Pedro Enciso <penciso@ucsd.edu>
2025-01-11 11:25:37 -0800

    display different button based on page

--- app/ui/chores/Header.tsx
+++ app/ui/chores/Header.tsx
@@ -14,11 +14,20 @@ export default function Header({ page }: { page: "chores" | "new" }) {
     <header className="mt-10 w-11/12 mx-auto flex flex-col gap-3">
       <TypographyH1>Enciso Chores</TypographyH1>
       <div className="flex gap-6 items-center">
-        <Link href="/chores/create">
-          <Button variant="outline" size="sm" className="w-fit">
-            Add a chore
-          </Button>
-        </Link>
+        {page === "chores" ? (
+          <Link href="/chores/create">
+            <Button variant="outline" size="sm" className="w-fit">
+              Add a chore
+            </Button>
+          </Link>
+        ) : null}
+        {page === "new" ? (
+          <Link href="/chores">
+            <Button variant="outline" size="sm" className="w-fit">
+              Back to chores
+            </Button>
+          </Link>
+        ) : null}
         {member_context.member ? (
           <div className="flex gap-2 items-center">
             <User className="h-5 w-5" />
